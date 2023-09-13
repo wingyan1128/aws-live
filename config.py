962d24e2684e57eb7ff08acdbd9586fb001129ff
wingyan1128 <126428734+wingyan1128@users.noreply.github.com>
2023-09-13 22:53:50 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "database-1.c4qlbl5ajqxe.us-east-1.rds.amazonaws.com" 
+customhost = "itp.c4qlbl5ajqxe.us-east-1.rds.amazonaws.com" 
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "itp" 
