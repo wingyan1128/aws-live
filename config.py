844109b91ad5d986585c13f7352e79644899edd3
wingyan1128 <126428734+wingyan1128@users.noreply.github.com>
2023-09-13 22:53:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 customhost = "database-1.c4qlbl5ajqxe.us-east-1.rds.amazonaws.com" 
 customuser = "aws_user"
 custompass = "Bait3273"
-customdb = "employee" 
+customdb = "itp" 
 custombucket = "phoonwingyan-employee" 
 customregion = "us-east-1"
 
