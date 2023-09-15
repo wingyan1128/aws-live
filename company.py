26a52cf82008235cf93aea5ed1e31d03270658a0
wingyan1128 <126428734+wingyan1128@users.noreply.github.com>
2023-09-16 00:42:15 +0800

    Update company.py

--- company.py
+++ company.py
@@ -208,6 +208,110 @@ def studRegister():
 
     return render_template('StudRegister.html', studRegisterSuccessfully = True)
 
+
+@app.route("/studLogin", methods=['GET', 'POST'])
+def studLogin():
+    studEmail = request.form['studEmail']
+    studIc = request.form['studIc']
+    #status = "Pending Approval"
+
+
+    fetch_student_sql = "SELECT * FROM student WHERE studEmail = %s"
+    #fetch_company_sql = "SELECT * FROM company WHERE status = %s"
+    cursor = db_conn.cursor()
+
+    if studEmail == "" and studIc == "":
+        return render_template('StudLogin.html', empty_field=True)
+
+    try:
+        cursor.execute(fetch_student_sql, (studEmail))
+        records = cursor.fetchall()
+
+        # cursor.execute(fetch_company_sql, (status))
+        # companyRecords = cursor.fetchall()
+
+        if records and records[0][4] != studIc:
+            return render_template('StudLogin.html', login_failed=True)
+        else:
+            return render_template('StudPage.html', student=records)
+
+    except Exception as e:
+        return str(e)
+
+    finally:
+        cursor.close()
+
+
+@app.route("/studPage", methods=['GET','POST'])
+def studPage():
+    # cohort = request.form['cohort']
+    # internPeriod = request.form['internPeriod']
+    # studName = request.form['studName']
+    # studId = request.form['studId']
+    # studIc = request.form['studIc']
+    # studGender = request.form['studGender']
+    # programme = request.form['programme']
+    #studEmail = request.form['studEmail']
+    # studContact = request.form['studContact']
+    # uniSupervisor = request.form['uniSupervisor']
+    # uniEmail = request.form['uniEmail']
+    companyName = request.form['companyName']
+    companyAllowance = request.form['companyAllowance']
+    companySvName = request.form['companySvName']
+    companySvEmail = request.form['companySvEmail']
+    studId = request.args.get('studId')
+    companyApForm = request.files['companyApForm']
+    parentAckForm = request.files['parentAckForm']
+    letterOIdt = request.files['letterOIdt']
+    hiredEvid = request.files['hiredEvid']
+    
+
+    #fetch_student_sql = "SELECT * FROM student WHERE studId = %s"
+    sql = "UPDATE student SET companyName = %s AND companyAllowance = %s AND companySvName = %s AND companySvEmail = %s WHERE studId = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        # cursor.execute(fetch_student_sql, (studId))
+        # records = cursor.fetchall()
+
+        # if records == "":
+        #     return render_template('studPage.html', invalid_error=True)
+        cursor.execute(sql, (companyName, companyAllowance,companySvName, companySvEmail, studId,))
+        db_conn.commit()
+       
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "stud-id-" + str(studId) + "_file.pnf"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading files to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=companyApForm)
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=parentAckForm)
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=letterOIdt)
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=hiredEvid)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+       
+    finally:
+        cursor.close()
+
+    print("all modification done...")
+    return render_template('StudPage.html')
+
+
 @app.route("/adminLogin", methods=['GET', 'POST'])
 def adminLogin():
     adminEmail = request.form['adminEmail']
@@ -325,6 +429,11 @@ def toCompanyRegister():
 @app.route("/toStudRegister")
 def toStudRegister():
     return render_template('StudRegister.html') 
+
+@app.route("/toStudLogin")
+def toStudLogin():
+    return render_template('StudLogin.html') 
+
 
 
 
